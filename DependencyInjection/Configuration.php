ededee311e7e4a2e34e8addb64ef3e778e6110de
François Pluchino <francois.pluchino@klipper.dev>
2020-05-31 11:22:45 +0200

    Rename and split the security oauth metadata bundle

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Bundle\SecurityOauthMetadataBundle\DependencyInjection;
+
+use Klipper\Bundle\SecurityBundle\DependencyInjection\NodeUtils;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your config files.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('klipper_security_oauth_metadata');
+        /** @var ArrayNodeDefinition $rootNode */
+        $rootNode = $treeBuilder->getRootNode();
+        $rootNode
+            ->addDefaultsIfNotSet()
+            ->append($this->getScopeLoaderNode())
+        ;
+
+        return $treeBuilder;
+    }
+
+    private function getScopeLoaderNode(): NodeDefinition
+    {
+        return NodeUtils::createArrayNode('scope_loader')
+            ->addDefaultsIfNotSet()
+            ->children()
+            ->arrayNode('metadata')
+            ->canBeDisabled()
+            ->end()
+            ->end()
+        ;
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+Klipper Security Oauth Metadata Bundle
+======================================
+
+The Security Oauth Metadata bundle add auto configurations for oauth with metadatas.
+
+Resources
+---------
+
+- [Documentation](https://doc.klipper.dev/bundles/security-oauth-metadata-bundle)
+- [Report issues](https://github.com/klipperdev/klipper/issues)
+  and [send Pull Requests](https://github.com/klipperdev/klipper/pulls)
+  in the [main Klipper repository](https://github.com/klipperdev/klipper)
